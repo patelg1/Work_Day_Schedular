168976fd0ddceec7e40dd209a9b1dc1309ceb58c
Gopal Patel <patelgopalk1@gmail.com>
2021-01-25 18:55:27 -0500

    Added array of times, modified for loop to create rows

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,17 +2,23 @@ var currentDate = dayjs().format("dddd, MMMM D, YYYY")
 $("#currentDay").text(currentDate);
 var currentHour = dayjs().format("H")
 
-for(var i = 9; i < 18; i++){
+var workHours = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"];
+    
+
+for(var i = 0; i < workHours.length; i++){
     var timeBlocks = $("<div>");
     timeBlocks.addClass("time-block");
     timeBlocks.addClass("row");    
+    
     var hourlyTime = $("<p>");
     hourlyTime.addClass("hour");
-    hourlyTime.addClass("col-1");
-    hourlyTime.text(i);
+    hourlyTime.addClass("col-1 pt-3");
+    hourlyTime.text(workHours[i]);
+    
     var messageText = $("<textarea>");
     messageText.addClass("description");
     messageText.addClass("col-10");
+   
     if (i < currentHour){
         messageText.addClass("past");
     }else if (i === currentHour){
@@ -20,13 +26,16 @@ for(var i = 9; i < 18; i++){
     }else{
         messageText.addClass("future");    
     }
+    
     var saveButton = $("<button>");
     saveButton.addClass("saveBtn");    
-    saveButton.addClass("col-1") ;
+    saveButton.addClass("col-1");    
     
     timeBlocks.append(hourlyTime);
     timeBlocks.append(messageText);
     timeBlocks.append(saveButton);
     $(".container").append(timeBlocks);
 }
+
+
 
