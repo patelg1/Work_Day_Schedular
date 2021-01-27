662fabeca91bbd3ae0b732c48268c551b13db843
Gopal Patel <patelgopalk1@gmail.com>
2021-01-27 15:22:54 -0500

    Modified local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,15 @@
-$(document).ready(function(){
-    var currentDate = dayjs().format("dddd, MMMM D, YYYY")
-    $("#currentDay").text(currentDate);
-    var currentHour = dayjs().format("H")
+// Variables to get current date and time from day.js
+var currentDate = dayjs().format("dddd, MMMM D, YYYY")
+$("#currentDay").text(currentDate);
+var currentHour = dayjs().format("H")
 
+$(document).ready(function(){
+    // Array to hold the hours in a workday
     var workHours = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"];
-        
+     
+    // Function to create the workday planner
     function createPlanner(){
+        // For loop to dynamically add rows and columns for hours and text area
         for(var i = 0; i < workHours.length; i++){
             var timeBlocks = $("<div>");
             timeBlocks.attr("id", i);
@@ -20,7 +24,8 @@ $(document).ready(function(){
             var messageText = $("<textarea>");
             messageText.addClass("description");
             messageText.addClass("col-10");
-        
+            
+            // Conditional statement to check if the current time is in the past, present or future
             if (i < currentHour){
                 messageText.addClass("past");
             }else if (i === currentHour){
@@ -40,13 +45,19 @@ $(document).ready(function(){
             $(".container").append(timeBlocks);
         }
     }
-
-    $(".saveBtn").click(function(){
-        var timeId = $(this).attr(".time-block");
-        var hourText = $(this).parent().children(".description");
-        localStorage.setItem(timeId, hourText);
+    // Save button on click function to save description to local storage
+    $(".saveBtn").click(function(event){
+        event.preventDefault();
+        var timeId = $("messageText")[i].value;
+        
+        localStorage.setItem("timeId", timeId);
 
     })
+
+    function getText(){
+        $("messageText")[i].value = localStorage.getItem("timeId");
+    }
     createPlanner();
+    getText();
 });
 
