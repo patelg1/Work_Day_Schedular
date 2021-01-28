701a1f95ff22b7706395db056e9b6d949492584c
Gopal Patel <patelgopalk1@gmail.com>
2021-01-28 15:47:54 -0500

    fixed setting to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,20 @@
+
+
 // Variables to get current date and time from day.js
 var currentDate = dayjs().format("dddd, MMMM D, YYYY")
 $("#currentDay").text(currentDate);
-var currentHour = dayjs().format("H")
+var currentHour = dayjs().format("h")
+console.log(currentHour);
+
+// Array to hold the hours in a workday
+var workHours = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"];
 
 $(document).ready(function(){
-    // Array to hold the hours in a workday
-    var workHours = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"];
-     
+        
     // Function to create the workday planner
     function createPlanner(){
         // For loop to dynamically add rows and columns for hours and text area
-        for(var i = 0; i < workHours.length; i++){
+        for(var i = 0; i < 9; i++){
             var timeBlocks = $("<div>");
             timeBlocks.attr("id", i);
             timeBlocks.addClass("time-block");
@@ -23,6 +27,7 @@ $(document).ready(function(){
             
             var messageText = $("<textarea>");
             messageText.addClass("description");
+            messageText.addClass(workHours[i]);
             messageText.addClass("col-10");
             
             // Conditional statement to check if the current time is in the past, present or future
@@ -33,7 +38,8 @@ $(document).ready(function(){
             }else{
                 messageText.addClass("future");    
             }
-            
+            console.log(messageText);
+            // Creating the save button
             var saveButton = $("<button>");
             saveButton.addClass("saveBtn");    
             saveButton.addClass("col-1");
@@ -43,21 +49,25 @@ $(document).ready(function(){
             timeBlocks.append(messageText);
             timeBlocks.append(saveButton);
             $(".container").append(timeBlocks);
+
+            var key = workHours[i];
+            var data = localStorage.getItem(key);
+            messageText.val(data);
         }
     }
+    createPlanner();
     // Save button on click function to save description to local storage
-    $(".saveBtn").click(function(event){
+    $(".saveBtn").on("click", function(event){
         event.preventDefault();
-        var timeId = $("messageText")[i].value;
+        var value = $(this).siblings(".description").val();
+        var hours = $(this).siblings(".hour").val();
         
-        localStorage.setItem("timeId", timeId);
+        localStorage.setItem(hours, value);
+        console.log(localStorage.getItem("#0"));
 
     })
 
-    function getText(){
-        $("messageText")[i].value = localStorage.getItem("timeId");
-    }
-    createPlanner();
-    getText();
+    
+    
 });
 
